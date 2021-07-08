4bc1771de7d8fc3a6109a0101e72adb5de8dc30a
Kim-Gwang-Min <gwang0622@naver.com>
2021-07-08 09:34:37 +0900

    7/8 get method setting

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -91,6 +91,7 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',

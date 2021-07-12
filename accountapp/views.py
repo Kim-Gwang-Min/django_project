9ef75492fece719dd630cd81b5002731387cc5f3
Kim-Gwang-Min <gwang0622@naver.com>
2021-07-12 10:13:40 +0900

    7/12 redirect setting

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -14,14 +16,9 @@ def hello_world(request):
         new_hello_world.text = temp  #models.py에 temp 넣기
         new_hello_world.save()  #models에 넣었으니 db에 저장
 
-        hello_world_list = HelloWorld.objects.all()  #DB의 모든 데이터를 가져옴
-
-
-        return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))    #accountapp안에 hello_world 라우터에 재연결 해라! 그래야 Post 요청을 계속 반복 안함!
 
     else:
         hello_world_list = HelloWorld.objects.all()   #DB의 모든 데이터를 가져옴
-
         return render(request, 'accountapp/hello_world.html',
                       context={'hello_world_list': hello_world_list})

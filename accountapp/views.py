96f2a5492e62058ad2e5260a09efb80ceca02415
Kim-Gwang-Min <gwang0622@naver.com>
2021-07-12 12:19:53 +0900

    7/12 redirect & Class AccountCreateView

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -20,5 +23,15 @@ def hello_world(request):
 
     else:
         hello_world_list = HelloWorld.objects.all()   #DB의 모든 데이터를 가져옴
+        hello_world_list.delete()
         return render(request, 'accountapp/hello_world.html',
                       context={'hello_world_list': hello_world_list})
+
+
+
+
+class AccountCreateView(CreateView):   #AccountCreateView라는 이름의 class를 만들 건데, CreatView를 상송받아라!
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')  #reverse와 무슨 차이냐면, class 안에서는 reverse_lazy를 해야함!
+    template_name = 'accountapp/create.html'  #create.html 라우터를 만듬

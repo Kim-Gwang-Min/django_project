c40cbe99ea68a684af7e73108272cc775b972b1d
Kim-Gwang-Min <gwang0622@naver.com>
2021-08-23 10:25:22 +0900

    8/23 subscribeapp

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
     path('projects/', include('projectapp.urls')),
+    path('subscribe/', include('subscribeapp.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  #이렇게 해줘야 프로필 이미지가 나온다
 
--- projectapp/models.py
+++ projectapp/models.py
@@ -7,8 +7,6 @@ class Project(models.Model):
     name = models.CharField(max_length=20, null=False)
     description = models.CharField(max_length=200, null=True) #null처리는 DB에서만 하는 것!
     image = models.ImageField(upload_to='project/', null=False)
-
     created_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self): #이건 장고 모델을 바꿔준게 아니라, 파이썬 안에서의 변화라 migration 안해도 됌
         return f'{self.name}'
